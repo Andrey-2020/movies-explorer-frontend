cac34bac3ad5b6773bd5b363cf9d31b6c1966b9e
Andrey Medvedev <andrei.medmedvedev@yandex.ru>
2021-11-29 04:18:35 +0300

    fix: переделал валидацию форм с помощью библиотеки yup, сами формы реализованы с помощью Formik

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,68 +1,79 @@
 import React, { useCallback } from "react";
 import { Link } from "react-router-dom";
+import * as yup from 'yup';
+import { Formik } from 'formik';
 import "./Login.css";
 
 function Login({ onLogin, clearErrors, loginError, setLoginError }) {
-    const [values, setValues] = React.useState({});
-    const [errors, setErrors] = React.useState({});
-    const [isValid, setIsValid] = React.useState(false);
+    const validationsSchema = yup.object().shape({
+        email: yup.string().typeError('Должно быть строкой').email('Введите верный email').required('Обязательно'),
+        password: yup.string().typeError('Должно быть строкой').min(8, 'Пароль должен содержать минимум 8 символов').required('Обязательно')
+    })
 
-    const handleChange = (event) => {
-        const target = event.target;
-        const name = target.name;
-        const value = target.value;
-        setValues({ ...values, [name]: value });
-        setErrors({ ...errors, [name]: target.validationMessage });
-        setIsValid(target.closest("form").checkValidity());
-    };
 
-    const resetForm = useCallback(
-        (newValues = {}, newErrors = {}, newIsValid = false) => {
-            setValues(newValues);
-            setErrors(newErrors);
-            setIsValid(newIsValid);
-        },
-        [setValues, setErrors, setIsValid]
-    );
-
-    function handleLogin(e) {
-        e.preventDefault();
+    function handleLogin(values) {
         onLogin({ email: values.email, password: values.password });
+
     }
     function handleClearErrors() {
-        resetForm();
         clearErrors();
     }
-    function handleChangeInput(e) {
-        if (loginError.length > 0) {
-            setLoginError("");
-        }
-        handleChange(e);
 
-    }
     return (
         <section className="login">
             <Link to="/" className="login__logo"></Link>
-            <h1 className="login__title">Рады видеть!</h1>
-            <form className="form form_type_login" name={`form-login`} onSubmit={handleLogin}>
-                <fieldset className="form__input-container form__input-container_type_login">
-                    <p className="form__text">E-mail</p>
-                    <input className="form__input form__input-login form__input_type_email" type="email" name="email"
-                        placeholder="E-mail" id="email-input" value={values.email} onChange={handleChangeInput} required />
-                    <span className="email-input-error form__input-error">{errors.email}</span>
-                    <p className="form__text">Пароль</p>
-                    <input className="form__input form__input-login form__input_type_password" type="password" name="password"
-                        placeholder="Пароль" id="password-input" value={values.password} onChange={handleChangeInput} required />
-                    <span className="password-input-error form__input-error">{errors.password}</span>
-                </fieldset>
-                <span className="login__error">{loginError}</span>
-                <button className={isValid ? "form__button form__button_type_login" : " form__button form__button_type_login form__button_invalid"} type="submit" aria-label="submit"  disabled={!isValid}>Войти</button>
-            </form>
+            <h2 className="login__title">Рады видеть!</h2>
+            <Formik
+                initialValues={{
+                    password: '',
+                    email: ''
+                }}
+                validateOnBlur={false}
+                onSubmit={(values) => { handleLogin(values) }}
+                validationSchema={validationsSchema}
+            >
+                {({ values, errors, touched, handleChange, handleBlur, setTouched, isValid, handleSubmit, dirty }) => (
+                    <form className={`form form_type_login`} onSubmit={handleSubmit} noValidate>
+                        <fieldset className="form__input-container form__input-container_type_login">
+                            <p className="form__text">E-mail</p>
+                            <input
+                                className={'form__input form__input-login form__input_type_email'}
+                                type={`email`}
+                                name={`email`}
+                                onChange={(e) => { handleChange(e); setTouched({ ...touched, [e.target.name]: true }) }}
+                                onBlur={handleBlur}
+                                value={values.email}
+                                placeholder="E-mail"
+                            />
+                            {touched.email && errors.email && <span className={'name-input-error form__input-error'}>{errors.email}</span>}
+
+                            <p className="form__text">Пароль</p>
+                            <input
+                                className={'form__input form__input-login form__input_type_password'}
+                                type={`password`}
+                                name={`password`}
+                                onChange={(e) => { handleChange(e); setTouched({ ...touched, [e.target.name]: true }) }}
+                                onBlur={handleBlur}
+                                value={values.password}
+                                placeholder="Пароль"
+                            />
+                            {touched.password && errors.password && <span className={'name-input-error form__input-error'}>{errors.password}</span>}
+                        </fieldset>
+                        <span className="login__error">{loginError}</span>
+                        <button
+                            disabled={!isValid || !dirty}
+                            type={`submit`}
+                            className={(isValid && !Object.values(values).includes('')) ? "form__button form__button_type_login" : " form__button form__button_type_login form__button_invalid"}
+                        >Войти</button>
+                    </form>
+                )}
+            </Formik>
             <div className="login__links">
                 <p className="login__answer">Ещё не зарегистрированы?</p>
                 <Link className="login__link" to="/signup" onClick={handleClearErrors}>Регистрация</Link>
             </div>
-        </section>
+        </section >
     )
+
 };
 export default Login;
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,68 +1,92 @@
-import React, { useCallback } from "react";
-import { Route, Redirect, Switch, useHistory, useLocation } from "react-router-dom";
+import React from "react";
 import { Link } from "react-router-dom";
+import * as yup from 'yup';
+import { Formik } from 'formik';
 import "./Register.css";
 
-function Register(props) {
-    const [values, setValues] = React.useState({});
-    const [errors, setErrors] = React.useState({});
-    const [isValid, setIsValid] = React.useState(false);
-
-    const handleChange = (event) => {
-        const target = event.target;
-        const name = target.name;
-        const value = target.value;
-        setValues({ ...values, [name]: value });
-
-        setErrors({ ...errors, [name]: target.validationMessage });
-
-        setIsValid(target.closest("form").checkValidity());
-    };
+function Register({ onRegister, clearErrors, registerError, setRegisterError }) {
+    const validationsSchema = yup.object().shape({
+        name: yup.string().typeError('Должно быть строкой').min(2, 'Имя должно содержать минимум 2 символа').max(30, 'Имя должно содержать максимум 30 символов')
+        .matches(/^[а-яА-Яa-zA-ZёË\- ]{1,}$/, 'Имя должно содержать только латиницу, кириллицу, пробел или дефис').required('Обязательно'),
+        email: yup.string().typeError('Должно быть строкой').email('Введите верный email').required('Обязательно'),
+        password: yup.string().typeError('Должно быть строкой').min(8, 'Пароль должен содержать минимум 8 символов').required('Обязательно')
+    })
 
 
-    function handleSubmit(e) {
-        e.preventDefault()
-        props.onRegister({ email: values.email, name: values.name, password: values.password });
+    function handleSubmit(values) {
+        onRegister({ email: values.email, name: values.name, password: values.password });
     }
-    function handleChangeInput(event) {
-        handleChange(event);
 
-    }
     function handleClearErrors() {
-        props.clearErrors();
-        resetForm()
+        clearErrors();
     }
-    const resetForm = useCallback(
-        (newValues = {}, newErrors = {}, newIsValid = false) => {
-            setValues(newValues);
-            setErrors(newErrors);
-            setIsValid(newIsValid);
-        },
-        [setValues, setErrors, setIsValid]
-    );
     return (
         <section className={'register'}>
-            <Link to="/" className="register__logo" />
-            <h2 className="register__title">Добро пожаловать!</h2>
             <div className={`register__container`}>
-                <form className={`form form_type_register`} name={`form-register`} onSubmit={handleSubmit}>
-                    <fieldset className="form__input-container form__input-container_type_register">
-                        <p className="form__text">Имя</p>
-                        <input className="form__input form__input-register form__input_type_username" type="name" name="name" placeholder="Имя"
-                            minLength="2" maxLength="30" id="name-input" value={values.name} onChange={handleChangeInput} pattern="[а-яА-Яa-zA-ZёË\- ]{1,}" required />
-                        <span className="name-input-error form__input-error">{errors.name}</span>
-                        <p className="form__text">E-mail</p>
-                        <input className="form__input form__input-register form__input_type_email" type="email" name="email"
-                            placeholder="E-mail" id="email-input" value={values.email} onChange={handleChangeInput} required />
-                        <span className="email-input-error form__input-error">{errors.email}</span>
-                        <p className="form__text">Пароль</p>
-                        <input className="form__input form__input-register form__input_type_password" type="password" name="password"
-                            placeholder="Пароль" id="password-input"  value={values.password} onChange={handleChangeInput} required />
-                        <span className="password-input-error form__input-error">{errors.password}</span>
-                    </fieldset>
-                    <span className="register__error">{props.registerError}</span>
-                    <button className={isValid ? "form__button form__button_type_register" : " form__button form__button_type_register form__button_invalid"} type="submit" aria-label="submit" disabled={!isValid}>Зарегистрироваться</button>
-                </form>
+                <Link to="/" className="register__logo" />
+                <h2 className="register__title">Добро пожаловать!</h2>
+                <Formik
+                    initialValues={{
+                        name: '',
+                        password: '',
+                        email: ''
+                    }}
+                    validateOnBlur={false}
+                    onSubmit={(values) => { handleSubmit(values) }}
+                    validationSchema={validationsSchema}
+                >
+                    {({ values, errors, handleChange, handleBlur, touched, setTouched, isValid, handleSubmit, dirty }) => (
+                        <form className={`form form_type_register`} onSubmit={handleSubmit} noValidate >
+
+                            <fieldset className="form__input-container form__input-container_type_register">
+                                <p className="form__text">Имя</p>
+                                <input
+                                    className={'form__input form__input-register form__input_type_username'}
+                                    type={`name`}
+                                    name={`name`}
+                                    onChange={(e) => { handleChange(e); setTouched({ ...touched, [e.target.name]: true }) }}
+                                    onBlur={handleBlur}
+                                    value={values.name}
+                                    placeholder="Имя"
+                                    required
+                                />
+
+                                {touched.name && errors.name && <span className={'name-input-error form__input-error'}>{errors.name}</span>}
+
+                                <p className="form__text">E-mail</p>
+                                <input
+                                    className={'form__input form__input-register form__input_type_email'}
+                                    type={`email`}
+                                    name={`email`}
+                                    onChange={(e) => { handleChange(e); setTouched({ ...touched, [e.target.name]: true }) }}
+                                    onBlur={handleBlur}
+                                    value={values.email}
+                                    placeholder="E-mail"
+                                />
+                                {touched.email && errors.email && <span className={'name-input-error form__input-error'}>{errors.email}</span>}
+
+                                <p className="form__text">Пароль</p>
+                                <input
+                                    className={'form__input form__input-register form__input_type_password'}
+                                    type={`password`}
+                                    name={`password`}
+                                    onChange={(e) => { handleChange(e); setTouched({ ...touched, [e.target.name]: true }) }}
+                                    onBlur={handleBlur}
+                                    value={values.password}
+                                    placeholder="Пароль"
+                                />
+                                {touched.password && errors.password && <span className={'name-input-error form__input-error'}>{errors.password}</span>}
+                                {console.log(touched)}
+                            </fieldset>
+                            <span className="register__error">{registerError}</span>
+                            <button
+                                disabled={!isValid || !dirty}
+                                type={`submit`}
+                                className={(isValid && dirty) ? "form__button form__button_type_register" : " form__button form__button_type_register form__button_invalid"}
+                            >Зарегистрироваться</button>
+                        </form>
+                    )}
+                </Formik>
                 <div className="register__links">
                     <p className="register__answer">Уже зарегистрированы?</p>
                     <Link className="register__link" to="/signin" onClick={handleClearErrors}>Войти</Link>
